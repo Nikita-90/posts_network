f2386790ce3366e564a1111600bcde77ca3a2555
Nikita-90 <boyko.nikita@gmail.com>
2017-12-17 19:14:42 +0200

    static_dirs on staticfiles

--- posts_network/settings.py
+++ posts_network/settings.py
@@ -135,9 +135,9 @@ EMAIL_TIMEOUT = 10
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
