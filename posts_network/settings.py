4b3168c4211878c92f8b63ca085e19787a0892c5
Nikita-90 <boyko.nikita@gmail.com>
2017-12-17 19:01:13 +0200

    staticfiles

--- posts_network/settings.py
+++ posts_network/settings.py
@@ -132,7 +132,7 @@ EMAIL_TIMEOUT = 10
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
